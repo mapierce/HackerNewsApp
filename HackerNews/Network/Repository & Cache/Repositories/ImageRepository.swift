5cab4062b7b4743ce0b54e5e599056f7a8111406
Matthew Pierce <mapierce@tcd.ie>
2020-08-06 18:35:19 +0100

    Image repository properly fetching now

--- HackerNews/Network/Repository & Cache/Repositories/ImageRepository.swift
+++ HackerNews/Network/Repository & Cache/Repositories/ImageRepository.swift
@@ -7,15 +7,25 @@
 
 import SwiftUI
 import Combine
-import LinkPresentation
+import SwiftSoup
 
 class ImageRepository: Repository, ObservableObject {
     
+    private struct Constants {
+        
+        static let propertyAttribute = "property"
+        static let imageValue = "og:image"
+        static let contentAttribute = "content"
+        
+    }
+
+    typealias Identifier = (id: Int, endpoint: URL?)
+    
     private let subject = PassthroughSubject<Image, Error>()
+    private let transport: Transport
     private let cache: ImageCache
-    private let metadataProvider = LPMetadataProvider()
     private let placeholderImageLoader: PlaceholderImageLoader
-    private var cancellables: Set<AnyCancellable> = []
+    private var cancellable: AnyCancellable?
     
     var publisher: AnyPublisher<Image, Error> {
         subject.eraseToAnyPublisher()
@@ -23,83 +33,70 @@ class ImageRepository: Repository, ObservableObject {
     
     // MARK: - Initializer
     
-    init(cache: ImageCache = ImageCache.shared, placeholderImageLoader: PlaceholderImageLoader = PlaceholderImageLoader.shared) {
+    init(
+        transport: Transport = URLSession.shared,
+        cache: ImageCache = ImageCache.shared,
+        placeholderImageLoader: PlaceholderImageLoader = PlaceholderImageLoader.shared
+    ) {
+        self.transport = transport
         self.cache = cache
         self.placeholderImageLoader = placeholderImageLoader
     }
 
     // MARK: - Repository methods
     
-    func fetch(by identifier: URL?, forceRefresh: Bool) {
-        print("-=-=-=-=- Fetching image for: \(identifier?.absoluteString ?? "no identifier provided")")
-        guard let identifier = identifier else {
-            print("-=-=-=-=- Identifier is nil, returning placeholder image")
-            subject.send(placeholderImageLoader.getNextPlaceholderImage())
-            return
-        }
-        if let existingImage = cache.read(id: identifier) {
-            print("-=-=-=-=- Have existing image for identifier: \(identifier.absoluteString)")
+    func fetch(by identifier: Identifier, forceRefresh: Bool) {
+        if let existingImage = cache.read(id: identifier.id) {
             subject.send(existingImage)
             return
         }
-        getMetadata(from: identifier)
-            .flatMap(getImage)
-            .sink { completion in
-                switch completion {
-                case .failure(let error):
-                    print("-=-=-=-=- Entire repo failure for: \(identifier.absoluteString)")
-                    print(error.localizedDescription)
-                case .finished:
-                    print("-=-=-=-=- Entire repo finished for: \(identifier.absoluteString)")
-                    break
-                }
-            } receiveValue: { [unowned self] image in
-                print("-=-=-=-=- Entire repo success for: \(identifier.absoluteString)")
-                self.cache.write(image, for: identifier)
+        guard let endpoint = identifier.endpoint, let imageURL = getImageURL(from: endpoint) else {
+            let image = placeholderImageLoader.getNextPlaceholderImage()
+            subject.send(image)
+            cache.write(image, for: identifier.id)
+            return
+        }
+        let request = URLRequest(url: imageURL)
+        cancellable = transport
+            .checkingStatusCode()
+            .send(request: request)
+            .tryMap({ [unowned self] (data, _) -> Image in try self.dataToImage(data) })
+            .catch({ [unowned placeholderImageLoader] _ -> Just<Image> in Just(placeholderImageLoader.getNextPlaceholderImage()) })
+            .sink(receiveValue: { [unowned self] image in
                 self.subject.send(image)
-            }
-            .store(in: &cancellables)
+                self.cache.write(image, for: identifier.id)
+            })
     }
     
     func clearCache() {
         cache.clear()
     }
     
-    // MARK: - Private methods
+    // MARK: - Public methods
     
-    private func getMetadata(from url: URL) -> Future<NSItemProvider, Error> {
-        return Future { [unowned self] promise in
-            self.metadataProvider.startFetchingMetadata(for: url) { metadata, error in
-                print("-=-=-=-=- Fetching metadata for: \(url.absoluteString)")
-                if let error = error {
-                    print("-=-=-=-=- Error fetching metadata for: \(url.absoluteString)")
-                    promise(.failure(error))
-                    return
-                }
-                guard let imageProvider = metadata?.imageProvider else {
-                    print("-=-=-=-=- No image provider metadata for: \(url.absoluteString)")
-                    promise(.failure(ImageError.noImageProvider))
-                    return
-                }
-                print("-=-=-=-=- Got metadata for: \(url.absoluteString)")
-                promise(.success(imageProvider))
-            }
-        }
+    func cancel() {
+        cancellable?.cancel()
     }
     
-    private func getImage(from itemProvider: NSItemProvider) -> Future<Image, Error> {
-        return Future { promise in
-            itemProvider.loadObject(ofClass: UIImage.self) { image, error in
-                print("-=-=-=-=- Loading image...")
-                guard let image = image as? UIImage else {
-                    print("-=-=-=-=- No image found")
-                    promise(.failure(ImageError.noImageData))
-                    return
-                }
-                print("-=-=-=-=- Returning image")
-                promise(.success(Image(uiImage: image)))
-            }
+    // MARK: - Private methods
+    
+    func getImageURL(from source: URL) -> URL? {
+        do {
+            let contents = try String(contentsOf: source)
+            let doc = try SwiftSoup.parse(contents)
+            let imageElements = try doc.head()?.getElementsByAttributeValue(Constants.propertyAttribute, Constants.imageValue)
+            guard let element = imageElements?.array().first,
+                  let imageStringURL = try? element.attr(Constants.contentAttribute),
+                  let imageURL = URL(string: imageStringURL) else { return nil }
+            return imageURL
+        } catch { return nil }
+    }
+    
+    func dataToImage(_ data: Data) throws -> Image {
+        guard let uiImage = UIImage(data: data) else {
+            throw ImageError.dataToImageFailure
         }
+        return Image(uiImage: uiImage)
     }
     
 }
