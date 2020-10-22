ed845ea958e69095a275f2e2b5e3440d5ef97c33
Matthew Pierce <mapierce@tcd.ie>
2020-10-22 17:46:59 +0100

    Added new image type for caching

---
+++ HackerNews/Models/ImageType.swift
@@ -0,0 +1,13 @@
+//
+//  ImageType.swift
+//  HackerNews
+//
+//  Created by Matthew Pierce on 22/10/2020.
+//
+
+import Foundation
+
+enum ImageType {
+    case local(String)
+    case remote(URL)
+}
